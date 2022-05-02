f707a829986b1b69815e30f15c8ee175d0a9a677
Tessa Porras <83438124+TessaP97@users.noreply.github.com>
2022-05-01 20:54:14 -0700

    Add files via upload

---
+++ Tessa-Porras-YellowBelt/easy-bay.js
@@ -0,0 +1,9 @@
+var plants = document.getElementById("plant-hover-pics")
+function plantHover(imgName) {
+    plants.src = "images/"+imgName
+}
+
+var plants2 = document.getElementById("plant-hover-pics")
+function plantHoverOut(imgName) {
+    plants2.src = "images/"+imgName
+}

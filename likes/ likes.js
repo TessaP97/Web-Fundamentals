58a467274ac58a56b3502c994947ede4b732723b
Tessa Porras <83438124+TessaP97@users.noreply.github.com>
2022-05-01 20:57:03 -0700

    Add files via upload

---
+++ likes/ likes.js
@@ -0,0 +1,17 @@
+var countElement = document.querySelector("#count")
+
+function add1() {
+    countElement.innerText++;
+}
+
+var count2Element = document.querySelector("#count2")
+
+function add2() {
+    count2Element.innerText++;
+}
+
+var count3Element = document.querySelector("#count3")
+
+function add3() {
+    count3Element.innerText++;
+}
